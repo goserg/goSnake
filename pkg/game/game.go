db57087ca0e56ed37345954683f27e30e4611fee
goserg <gor-serg@bk.ru>
2023-12-21 08:48:56 +0300

    add debug fps info

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,6 +1,10 @@
 package game
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
 
 type Game struct {
 }
@@ -16,6 +20,8 @@ func (g Game) Update() error {
 }
 
 func (g Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %d", int(ebiten.ActualFPS())))
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %d", int(ebiten.ActualTPS())), 0, 10)
 }
 
 func (g Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
