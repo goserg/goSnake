cfc0d48f19fc64f834b6a1f31842bffa9e5f214a
goserg <gor-serg@bk.ru>
2023-12-22 22:11:18 +0300

    add snake field class

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,174 +1,48 @@
 package game
 
 import (
-	"errors"
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"goSnake/pkg/config"
-	"goSnake/pkg/image_manager"
 	"goSnake/pkg/input"
-	"goSnake/pkg/snake"
-	"goSnake/pkg/utils/vector"
-	"image/color"
-	"math/rand"
-	"time"
+	snakeField "goSnake/pkg/snake_field"
 )
 
-const tileSize = 32
-
 type Game struct {
-	speed int
-
-	grid            *ebiten.Image
-	queuedDirection int
-	direction       int
-	snake           *snake.Snake
-
-	foodPos vector.Vector
-
-	mainTicker *time.Ticker
+	snakeField *snakeField.SnakeField
 }
 
 func New() *Game {
 	var g Game
-
-	g.speed = 10
-	g.grid = image_manager.Grid()
-	g.snake = snake.New(vector.Vector{
-		X: 32 * 5,
-		Y: 32 * 5,
-	})
-	g.mainTicker = time.NewTicker(calcTick(g.speed))
-	g.newFood()
+	g.snakeField = snakeField.New()
+	g.snakeField.DeathCallback = g.OnDeath
 	return &g
 }
 
 func (g *Game) Update() error {
 	input.Update()
 
-	switch g.direction {
-	case input.Left, input.Right:
-		if input.DirectionV() != input.No {
-			g.queuedDirection = input.DirectionV()
-		}
-	case input.Up, input.Down:
-		if input.DirectionH() != input.No {
-			g.queuedDirection = input.DirectionH()
-		}
-	}
-
-	select {
-	case <-g.mainTicker.C:
-		if g.queuedDirection != input.No {
-			g.direction = g.queuedDirection
-		}
-		if g.direction == input.No {
-			g.direction = input.Right
-		}
-		var newPos vector.Vector
-		switch g.direction {
-		case input.Left:
-			newPos = vector.Vector{
-				X: g.snake.Pos.X - 32,
-				Y: g.snake.Pos.Y,
-			}
-		case input.Right:
-			newPos = vector.Vector{
-				X: g.snake.Pos.X + 32,
-				Y: g.snake.Pos.Y,
-			}
-		case input.Up:
-			newPos = vector.Vector{
-				X: g.snake.Pos.X,
-				Y: g.snake.Pos.Y - 32,
-			}
-		case input.Down:
-			newPos = vector.Vector{
-				X: g.snake.Pos.X,
-				Y: g.snake.Pos.Y + 32,
-			}
-		}
-		switch {
-		case newPos.X < 0:
-			newPos.X = config.FieldWidth - config.TileSize
-		case newPos.Y < 0:
-			newPos.Y = config.FieldHeight - config.TileSize
-		case newPos.X+config.TileSize > config.FieldWidth:
-			newPos.X = 0
-		case newPos.Y+config.TileSize > config.FieldHeight:
-			newPos.Y = 0
-		}
-
-		s := g.snake.Next
-		for s != nil {
-			if s.Pos == newPos {
-				return errors.New("you died")
-			}
-			s = s.Next
-		}
-
-		g.snake.Move(newPos)
-		if g.snake.Pos == g.foodPos {
-			g.snake.Grow()
-			g.newFood()
-		}
-	default:
+	if err := g.snakeField.Update(); err != nil {
+		return err
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	snakeField := ebiten.NewImage(config.FieldWidth+1, config.FieldHeight+1)
-	snakeField.Fill(color.RGBA{10, 10, 10, 0})
-
-	snakeField.DrawImage(g.grid, nil)
-	if g.snake != nil {
-		g.snake.Draw(snakeField, calcTick(g.speed))
-	}
-
-	var foodDrawOptions ebiten.DrawImageOptions
-	foodDrawOptions.GeoM.Translate(g.foodPos.X, g.foodPos.Y)
-	snakeField.DrawImage(image_manager.Food(), &foodDrawOptions)
-
 	var snakeFieldDrawingOptions ebiten.DrawImageOptions
 	snakeFieldDrawingOptions.GeoM.Translate(config.FieldLeft, config.FieldTop)
-	screen.DrawImage(snakeField, &snakeFieldDrawingOptions)
+	g.snakeField.Draw(screen, &snakeFieldDrawingOptions)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %d", int(ebiten.ActualFPS())))
 	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %d", int(ebiten.ActualTPS())), 0, 10)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("speed: %d", g.speed), 0, 20)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
-func (g *Game) newFood() {
-	newFoodPos := vector.Vector{
-		X: float64(rand.Intn(config.FieldWidth/config.TileSize) * config.TileSize),
-		Y: float64(rand.Intn(config.FieldHeight/config.TileSize) * config.TileSize),
-	}
-
-	s := g.snake
-	for s != nil {
-		if s.Pos == newFoodPos {
-			fmt.Println("collision")
-			newFoodPos = vector.Vector{
-				X: float64(rand.Intn(config.FieldWidth/config.TileSize) * config.TileSize),
-				Y: float64(rand.Intn(config.FieldHeight/config.TileSize) * config.TileSize),
-			}
-		}
-		s = s.Next
-	}
-	g.foodPos = newFoodPos
-}
-
-func (g *Game) SpeedUp() {
-	g.speed++
-	g.mainTicker = time.NewTicker(calcTick(g.speed))
-}
-
-func calcTick(speed int) time.Duration {
-	return (time.Second) / time.Duration(speed)
+func (g *Game) OnDeath() {
+	g.snakeField = snakeField.New()
+	g.snakeField.DeathCallback = g.OnDeath
 }
---
+++ pkg/snake_field/snake_field.go
@@ -0,0 +1,163 @@
+package snake_field
+
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"goSnake/pkg/config"
+	"goSnake/pkg/image_manager"
+	"goSnake/pkg/input"
+	"goSnake/pkg/snake"
+	"goSnake/pkg/utils/vector"
+	"image/color"
+	"math/rand"
+	"time"
+)
+
+type SnakeField struct {
+	speed int
+
+	grid            *ebiten.Image
+	queuedDirection int
+	direction       int
+	snake           *snake.Snake
+
+	foodPos vector.Vector
+
+	mainTicker *time.Ticker
+
+	DeathCallback func()
+}
+
+func New() *SnakeField {
+	var snakeField SnakeField
+
+	snakeField.speed = 10
+	snakeField.grid = image_manager.Grid()
+	snakeField.snake = snake.New(vector.Vector{
+		X: 32 * 5,
+		Y: 32 * 5,
+	})
+	snakeField.mainTicker = time.NewTicker(calcTick(snakeField.speed))
+	snakeField.newFood()
+	return &snakeField
+}
+
+func (sf *SnakeField) Update() error {
+	input.Update()
+
+	switch sf.direction {
+	case input.Left, input.Right:
+		if input.DirectionV() != input.No {
+			sf.queuedDirection = input.DirectionV()
+		}
+	case input.Up, input.Down:
+		if input.DirectionH() != input.No {
+			sf.queuedDirection = input.DirectionH()
+		}
+	}
+
+	select {
+	case <-sf.mainTicker.C:
+		if sf.queuedDirection != input.No {
+			sf.direction = sf.queuedDirection
+		}
+		if sf.direction == input.No {
+			sf.direction = input.Right
+		}
+		var newPos vector.Vector
+		switch sf.direction {
+		case input.Left:
+			newPos = vector.Vector{
+				X: sf.snake.Pos.X - 32,
+				Y: sf.snake.Pos.Y,
+			}
+		case input.Right:
+			newPos = vector.Vector{
+				X: sf.snake.Pos.X + 32,
+				Y: sf.snake.Pos.Y,
+			}
+		case input.Up:
+			newPos = vector.Vector{
+				X: sf.snake.Pos.X,
+				Y: sf.snake.Pos.Y - 32,
+			}
+		case input.Down:
+			newPos = vector.Vector{
+				X: sf.snake.Pos.X,
+				Y: sf.snake.Pos.Y + 32,
+			}
+		}
+		switch {
+		case newPos.X < 0:
+			newPos.X = config.FieldWidth - config.TileSize
+		case newPos.Y < 0:
+			newPos.Y = config.FieldHeight - config.TileSize
+		case newPos.X+config.TileSize > config.FieldWidth:
+			newPos.X = 0
+		case newPos.Y+config.TileSize > config.FieldHeight:
+			newPos.Y = 0
+		}
+
+		s := sf.snake.Next
+		for s != nil {
+			if s.Pos == newPos {
+				sf.DeathCallback()
+				return nil
+			}
+			s = s.Next
+		}
+
+		sf.snake.Move(newPos)
+		if sf.snake.Pos == sf.foodPos {
+			sf.snake.Grow()
+			sf.newFood()
+		}
+	default:
+	}
+	return nil
+}
+
+func (sf *SnakeField) Draw(screen *ebiten.Image, opts *ebiten.DrawImageOptions) {
+	snakeField := ebiten.NewImage(config.FieldWidth+1, config.FieldHeight+1)
+	snakeField.Fill(color.RGBA{R: 10, G: 10, B: 10})
+
+	snakeField.DrawImage(sf.grid, nil)
+	if sf.snake != nil {
+		sf.snake.Draw(snakeField, calcTick(sf.speed))
+	}
+
+	var foodDrawOptions ebiten.DrawImageOptions
+	foodDrawOptions.GeoM.Translate(sf.foodPos.X, sf.foodPos.Y)
+	snakeField.DrawImage(image_manager.Food(), &foodDrawOptions)
+
+	screen.DrawImage(snakeField, opts)
+}
+
+func (sf *SnakeField) newFood() {
+	newFoodPos := vector.Vector{
+		X: float64(rand.Intn(config.FieldWidth/config.TileSize) * config.TileSize),
+		Y: float64(rand.Intn(config.FieldHeight/config.TileSize) * config.TileSize),
+	}
+
+	s := sf.snake
+	for s != nil {
+		if s.Pos == newFoodPos {
+			fmt.Println("collision")
+			newFoodPos = vector.Vector{
+				X: float64(rand.Intn(config.FieldWidth/config.TileSize) * config.TileSize),
+				Y: float64(rand.Intn(config.FieldHeight/config.TileSize) * config.TileSize),
+			}
+		}
+		s = s.Next
+	}
+	sf.foodPos = newFoodPos
+}
+
+func (sf *SnakeField) SpeedUp() {
+	sf.speed++
+	sf.mainTicker = time.NewTicker(calcTick(sf.speed))
+}
+
+func calcTick(speed int) time.Duration {
+	return (time.Second) / time.Duration(speed)
+}
